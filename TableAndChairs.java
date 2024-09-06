0d44cf0423cd99367671a7e32188081b79cf5b2c
JoshuaDeej <jcammarata2005@gmail.com>
2024-09-05 20:47:46 -0400

    Initial Commit

---
+++ TableAndChairs.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class TableAndChairs {
+    public static void main(String[] args) {
+        System.out.println("                      ");
+        System.out.println("X                      X");
+        System.out.println("X                      X");
+        System.out.println("X      XXXXXXXXXX      X");
+        System.out.println("XXXXX  X        X  XXXXX");
+        System.out.println("X   X  X        X  X   X");
+        System.out.println("X   X  X        X  X   X");
+        System.out.println("                        ");
+    }
+}
