dcd9bd8e04b3b3c6849eeb6015fa96438091ac8a
JoshuaDeej <jcammarata2005@gmail.com>
2024-09-29 16:19:55 -0400

    Update

---
+++ C_Sharp_Programming_COP2360_1/Module_3/CaseNumber.cs
@@ -0,0 +1,53 @@
+﻿public class Customer
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public int PhoneNumber { get; set; }
+
+    public Customer(string firstName, string lastName, int phoneNumber)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        PhoneNumber = phoneNumber;
+    }
+
+    public void DisplayName()
+    {
+        Console.WriteLine($"Name: {FirstName} {LastName}");
+    }
+    public void DisplayPhoneNumber()
+    {
+        Console.WriteLine($"Phone Number: {PhoneNumber}");
+    }
+}
+
+// Issue class inheriting from Customer
+public class Case : Customer
+{
+    public int CaseId { get; set; }
+
+    public Case(string firstName, string lastName, int phoneNumber, int caseId)
+        : base(firstName, lastName, phoneNumber)
+    {
+        CaseId = caseId;
+    }
+
+    public void DisplayCaseInfo()
+    {
+        DisplayName();
+        DisplayPhoneNumber();
+        Console.WriteLine($"Case ID: {CaseId}");
+    }
+}
+
+// Main program
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an instance of Case
+        Case customer = new Case("John", "Doe", 1111111111, 123456);
+        customer.DisplayCaseInfo();
+    }
+}
